46be16c1b6fa1deb4d3a1f73c8e1a7dc4722a008
shanthis2790 <shanthis2790@gmail.com>
2024-08-19 22:16:39 +0530

    Add files via upload
    Week 1 Home Assignment

---
+++ practice/Chrome.java
@@ -0,0 +1,23 @@
+package day1.practice;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Float version=91.0f;
+ System.out.println (version+"f");
+ String developer="Google";
+ System.out.println(developer);
+ boolean isBeta= false;
+ System.out.println(isBeta);
+ int releaseYear= 2008;
+ System.out.println(releaseYear);
+ char shortcutKey='C';
+ System.out.println(shortcutKey);
+ 
+ 
+ 
+ 
+	}
+
+}
